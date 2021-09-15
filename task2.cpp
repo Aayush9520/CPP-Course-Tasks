a5163b7f8e0b3a8e5edfcf8a2234f21ca238b5e6
Aayush9520 <90785482+Aayush9520@users.noreply.github.com>
2021-09-15 23:47:14 +0530

    Product
    This CPP code gives product of two numbers.

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,product;
+    cout << "Enter the first number : ";
+    cin >>a;
+    cout << "Enter the second number : ";
+    cin >>b;
+
+    product = a*b;
+    cout << "The product of two numbers is : " <<product;
+    return 0;
+}
